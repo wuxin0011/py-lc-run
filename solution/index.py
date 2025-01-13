9b4a9cc21361f9d9e0b942bddda8ea8e5058d018
wuxin0011 <2191377759@qq.com>
2025-01-14 01:53:26 +0800

    init

--- solution/index.py
+++ solution/index.py
@@ -1,7 +1,7 @@
 from generator.contest import parse_problem_by_urls
 
 '''
-获取当前马上进行的周赛|双周赛 如果距离比赛开始超过半个小时以上程序自动退出
+输入链接可以拉取改题目，也可以批量操作 按下回车两次启动
 '''
 if __name__ == '__main__':
     parse_problem_by_urls("__test__")
